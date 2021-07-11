5121b515fbcda7d37597a6db9fc822f2a3fbdec2
Kevin Sandom <kjsandom@gmail.com>
2021-07-11 16:27:05 +0200

    Lots of consistentcy and testing improvements

--- shest/unit/00-createEnv/00-createData.sh
+++ shest/unit/00-createEnv/00-createData.sh
@@ -3,19 +3,7 @@
 
 . "$SHEST_SCRIPT" "--doNothing"
 
-passCode=0
-mkdir -p data/{context1,context2} projects/example cache
-exitCode1=$?
+result="$(./misc/createDuseTestData)"
+exitCode="$?"
 
-echo 1 > data/context1/a
-echo 2 > data/context1/b
-echo 3 > data/context2/c
-echo 4 > data/context2/d
-
-exitCode2=$?
-
-if [ "$exitCode1" == "$passCode" ] && [ "$exitCode2" == "$passCode" ] ; then
-    pass "Great!"
-else
-    fail "Got exitCode1=$exitCode1 and exitCode2=$exitCode2. Needed $passCode for both."
-fi
+expect_exitCode 0 
