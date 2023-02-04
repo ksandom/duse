f9f5551287429006ffaf850d2106fb50862c2578
Kevin Sandom <kjsandom@gmail.com>
2023-02-04 15:05:06 +1300

    Shellcheck now passes

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -1,12 +1,12 @@
 # Include all includes.
 
-. $duseLib/global.sh
-. $duseLib/context.sh
-. $duseLib/source.sh
-. $duseLib/cache.sh
-. $duseLib/workspace.sh
-. $duseLib/entry.sh
-. $duseLib/useItem.sh
-. $duseLib/cacheItem.sh
-. $duseLib/parameters.sh
-. $duseLib/system.sh
+. "$duseLib/global.sh"
+. "$duseLib/context.sh"
+. "$duseLib/source.sh"
+. "$duseLib/cache.sh"
+. "$duseLib/workspace.sh"
+. "$duseLib/entry.sh"
+. "$duseLib/useItem.sh"
+. "$duseLib/cacheItem.sh"
+. "$duseLib/parameters.sh"
+. "$duseLib/system.sh"
